2582314dc9cf234c2e4755980a3a64c4deb1cce4
c0ckt0il <161923732+c0ckt0il@users.noreply.github.com>
2025-08-21 16:59:49 +0900

    Update DemoApplication.java
    private static final Logger logger = LoggerFactory.getLogger("OTLP");

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-    private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+    private static final Logger logger = LoggerFactory.getLogger("OTLP");
+
     
     public static void main(String[] args) {
         logger.info("Application starting...COCKTAIL LOG SERVICE TEST");  // 애플리케이션 시작 로그
