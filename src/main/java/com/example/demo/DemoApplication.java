45df561787b9c54cf9bcc93f3e602af6689a8b52
c0ckt0il <161923732+c0ckt0il@users.noreply.github.com>
2025-08-21 16:17:32 +0900

    log service logger setting add

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+    
     public static void main(String[] args) {
+        logger.info("Application starting...COCKTAIL LOG SERVICE TEST");  // 애플리케이션 시작 로그
         SpringApplication.run(DemoApplication.class, args);
+        logger.info("Application started successfully.");
     }
 
 }
