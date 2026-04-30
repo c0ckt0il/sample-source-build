b345508812253f1331e0f5d85d1b14d2ce3ed5be
c0ckt0il <161923732+c0ckt0il@users.noreply.github.com>
2026-04-30 14:32:24 +0900

    Add @Controller annotation to DemoApplication

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class DemoApplication {
 
     public static void main(String[] args) {
